e2a730c06104378a273a9e14274607fc6f1f7861
serhio4smotrov <ser19061995@gmail.com>
2024-09-08 14:35:18 +0300

    initial

---
+++ module_2_4.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+is_prime = True
+for i in numbers:
+    if i == 1:
+        continue
+    for j in range(2, i):
+        if i % j != 0:
+            is_prime = True
+        else:
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+print(f'Простые числа {primes}')
+print(f'Непростые числа {not_primes}')
+
