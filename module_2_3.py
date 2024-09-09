e831d33f03033d97bd9cdc96167b4cd7a29724dc
serhio4smotrov <ser19061995@gmail.com>
2024-09-09 22:22:18 +0300

    initial

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,11 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 0, 7, -6, 5]
-i = -1
+my_list = [42, 69, 322, 13, 0, 99, 5, -9, 0, 7, 6, 5]
+i = 0
 while i < len(my_list):
-    i += 1
     if my_list[i] > 0:
         print(my_list[i])
+        i += 1
     elif my_list[i] == 0:
+        i += 1
         continue
     else:
         break
