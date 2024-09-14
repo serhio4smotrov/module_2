f6b8202a31b85b7678c48211829d8717345bce07
serhio4smotrov <ser19061995@gmail.com>
2024-09-14 21:18:44 +0300

    initial

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,12 +1,7 @@
 n = int(input('Пароль первой вставки: '))
-summ = []
 result = ''
-if 3 <= n <= 20:
-    for i in range(1,n):
-        for j in range(1,n):
-            if n % (i + j) == 0 and summ.count(sorted([i, j])) < 1 and i != j:
-                summ.append(sorted([i, j]))
-                result = result + f'{i},{j}  '
-    print(f'Пароль второй вставки: {result}')
-else:
-    print('Нет такого значения на первой вставке')
+for i in range(1, n):
+     for j in range(i + 1, n):
+        if n % (i + j) == 0:
+            result = result + f'{i},{j}  '
+print(f'Пароль второй вставки: {result}')
